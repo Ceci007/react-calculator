c6af0d89b0dfd164f918bd0b4f2831936059bb90
solbencas <cecibenitezca@gmail.com>
2021-02-02 13:08:58 -0300

    Fix linters errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Big from 'big.js';
 import PropTypes from 'prop-types';
+import "./App.css";
 
 const testBig = new Big('4.321e+4');
 const bigMsg = `New Big('4.321e+4') is: ${testBig}`;
@@ -8,7 +9,7 @@ const bigMsg = `New Big('4.321e+4') is: ${testBig}`;
 function App() {
   return (
     <div className="App">
-      <Counter  name = { 'testName' }/>
+      <Counter name="testName" />
     </div>
   );
 }
@@ -16,9 +17,9 @@ function App() {
 class Counter extends React.Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
-      count: 0
+      count: 0,
     };
 
     this.increment = this.increment.bind(this);
@@ -28,39 +29,54 @@ class Counter extends React.Component {
 
   reset() {
     this.setState({
-      count: 0
+      count: 0,
     });
   }
 
   increment() {
     this.setState(state => ({
-      count: state.count + 1
+      count: state.count + 1,
     }));
   }
 
   decrement() {
     this.setState(state => ({
-      count: state.count - 1
+      count: state.count - 1,
     }));
   }
 
   render() {
+    const { count } = this.state;
+    const { name } = this.props;
+
     return (
       <div>
-        <button className='inc' onClick={this.increment}>Increment!</button>
-        <button className='dec' onClick={this.decrement}>Decrement!</button>
-        <button className='reset' onClick={this.reset}>Reset</button>
-        <h1>Current Count: {this.state.count} </h1>
-        <h3>Prop name: {this.props.name} </h3>
-        <h3>{bigMsg} </h3>
+        <button type="button" className="inc" onClick={this.increment}>Increment!</button>
+        <button type="button" className="dec" onClick={this.decrement}>Decrement!</button>
+        <button type="button" className="reset" onClick={this.reset}>Reset</button>
+        <h1>
+          Current Count:
+          {count}
+        </h1>
+        <h3>
+          Prop name:
+          {name}
+        </h3>
+        <h3>
+          {bigMsg}
+          {' '}
+        </h3>
       </div>
     );
   }
-};
-
+}
 
 Counter.propTypes = {
-  name: PropTypes.string
+  name: PropTypes.string,
+};
+
+Counter.defaultProps = {
+  name: '',
 };
 
 export default App;
