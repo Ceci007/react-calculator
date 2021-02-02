2bc51683eed1dcb05aaabd869f1dba4750638f1e
solbencas <cecibenitezca@gmail.com>
2021-02-02 19:04:15 -0300

    Modify ButtonPannel.js

--- src/components/ButtonPannel.js
+++ src/components/ButtonPannel.js
@@ -2,38 +2,62 @@ import React from 'react';
 import '../index.css';
 import Button from './Button';
 
-const ButtonPannel = () => (
-  <div>
+function ButtonPannel() {
+  const defaultSize = 'simple';
+  const defaultColor = 'gray';
+  const colorSecundary = 'orange';
+  const doubleSize = 'double';
+  const propsBtns = [{ name: 'AC', size: defaultSize, color: defaultColor },
+    { name: '+/-', size: defaultSize, color: defaultColor },
+    { name: '%', size: defaultSize, color: defaultColor },
+    { name: '÷', size: defaultSize, color: colorSecundary },
+    { name: '7', size: defaultSize, color: defaultColor },
+    { name: '8', size: defaultSize, color: defaultColor },
+    { name: '9', size: defaultSize, color: defaultColor },
+    { name: 'X', size: defaultSize, color: colorSecundary },
+    { name: '4', size: defaultSize, color: defaultColor },
+    { name: '5', size: defaultSize, color: defaultColor },
+    { name: '6', size: defaultSize, color: defaultColor },
+    { name: '-', size: defaultSize, color: colorSecundary },
+    { name: '1', size: defaultSize, color: defaultColor },
+    { name: '2', size: defaultSize, color: defaultColor },
+    { name: '3', size: defaultSize, color: defaultColor },
+    { name: '+', size: defaultSize, color: colorSecundary },
+    { name: '0', size: doubleSize, color: defaultColor },
+    { name: '.', size: defaultSize, color: defaultColor },
+    { name: '=', size: defaultSize, color: colorSecundary }];
+
+  const createPannelLine = (propsBtns, line) => {
+    const result = [];
+    let i = 0;
+    let index = line * 4;
+    while (i < 4 && index < (propsBtns.length)) {
+      result.push(<Button
+        key={index + 1}
+        name={propsBtns[index].name}
+        color={propsBtns[index].color}
+        size={propsBtns[index].size}
+      />);
+      i += 1;
+      index += 1;
+    }
+
+    return (
+      <div>
+        { result.map(btn => btn) }
+      </div>
+    );
+  };
+
+  return (
     <div>
-      <Button name="AC" size="simple" color="gray" />
-      <Button name="+/-" size="simple" color="gray" />
-      <Button name="%" size="simple" color="gray" />
-      <Button name="÷" size="simple" color="orange" />
+      { createPannelLine(propsBtns, 0) }
+      { createPannelLine(propsBtns, 1) }
+      { createPannelLine(propsBtns, 2) }
+      { createPannelLine(propsBtns, 3) }
+      { createPannelLine(propsBtns, 4) }
     </div>
-    <div>
-      <Button name="7" size="simple" color="gray" />
-      <Button name="8" size="simple" color="gray" />
-      <Button name="9" size="simple" color="gray" />
-      <Button name="X" size="simple" color="orange" />
-    </div>
-    <div>
-      <Button name="4" size="simple" color="gray" />
-      <Button name="5" size="simple" color="gray" />
-      <Button name="6" size="simple" color="gray" />
-      <Button name="-" size="simple" color="orange" />
-    </div>
-    <div>
-      <Button name="1" size="simple" color="gray" />
-      <Button name="2" size="simple" color="gray" />
-      <Button name="3" size="simple" color="gray" />
-      <Button name="+" size="simple" color="orange" />
-    </div>
-    <div>
-      <Button name="0" size="double" color="gray" />
-      <Button name="." size="simple" color="gray" />
-      <Button name="=" size="simple" color="orange" />
-    </div>
-  </div>
-);
+  );
+}
 
 export default ButtonPannel;
