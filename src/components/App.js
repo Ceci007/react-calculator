55cb9db054884572cf8711840ce47fdb769ddd6d
solbencas <cecibenitezca@gmail.com>
2021-02-08 17:01:15 -0300

    Set state and event handlers in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
 import Display from './Display';
 import ButtonPannel from './ButtonPannel';
-// eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 import '../index.css';
 
-const App = () => (
-  <div className="calculator">
-    <Display />
-    <ButtonPannel />
-  </div>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(operation) {
+    const stateCalc = calculate(this.state, operation);
+    this.setState(stateCalc);
+  }
+
+  render() {
+    const { total, next, operation } = this.state;
+    let display = '0';
+    if (next) {
+      display = next;
+    } else if ((operation && operation !== '=') && total) {
+      display = operation;
+    } else {
+      display = total || '0';
+    }
+
+    return (
+      <div className="calculator">
+        <Display result={display} />
+        <ButtonPannel onClick={nameBtn => this.handleClick(nameBtn)} />
+      </div>
+    );
+  }
+} 
 
 export default App;
