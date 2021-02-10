226aa00df5cad4f7e8092001f3abf6d35f61da7c
solbencas <cecibenitezca@gmail.com>
2021-02-10 12:01:01 -0300

    Setup jest and pass first test in App.test.js

---
+++ test/components/App.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from '../../src/components/App';
+
+describe('App component should ', () => {
+  test('should render ButtonPanel and Display components', () => {
+    const app = renderer.create(<App />);
+    expect(app.toJSON()).toMatchSnapshot();
+  });
+});
+
+
+
+
+
+
